834ce3a187eb5d27105c1f460132d09533fad17c
Helena Douroucas <helenadouroucas@gmail.com>
2023-07-18 03:22:21 -0300

    runge-kutta funcionando em todos os sistemas

--- pendulo.py
+++ pendulo.py
@@ -89,29 +89,6 @@ def verlet_method(t0, tf, dt, x0, v0, f):
         E[i] = 0.5 * m * (v[i]*L)**2 + m*gr*L * (1-np.cos(x[i]))
     return t, x, E
 
-def runge_kutta_method(t0, tf, dt, x0, v0, f, g):
-    num_steps = int((tf - t0) / dt) + 1
-    t = np.linspace(t0, tf, num_steps)
-    x = np.zeros_like(t)
-    v = np.zeros_like(t)
-    E = np.zeros_like(t)
-    x[0] = x0
-    v[0] = v0
-    E[0] = 0.5 * m * (v[0]*L)**2 + m*gr*L * (1-np.cos(x[0]))
-    for i in range(1, num_steps):
-        k11 = g(t[i-1], v[i-1])
-        k12 = f(t[i-1], x[i-1])
-        k21 = g(t[i-1] + 0.5*dt, v[i-1] + 0.5*k11*dt)
-        k22 = f(t[i-1] + 0.5*dt, x[i-1] + 0.5*k12*dt)
-        k31 = g(t[i-1] + 0.5*dt, v[i-1] + 0.5*k21*dt)
-        k32 = f(t[i-1] + 0.5*dt, x[i-1] + 0.5*k22*dt)
-        k41 = g(t[i-1] + dt, v[i-1] + k31*dt)
-        k42 = f(t[i-1] + dt, x[i-1] + k32*dt)
-        v[i] = v[i-1] + (k12 + 2*k22 + 2*k32 + k42) * dt / 6
-        x[i] = x[i-1] + (k11 + 2*k21 + 2*k31 + k41) * dt / 6
-        E[i] = 0.5 * m * (v[i]*L)**2 + m*gr*L * (1-np.cos(x[i]))
-    return t, x, E
-
 def leapfrog_method(t0, tf, dt, x0, v0, f):
     num_steps = int((tf - t0) / dt) + 1
     t = np.linspace(t0, tf, num_steps)
@@ -133,7 +110,7 @@ def f(t, x): #v'=w'= f(t,x)
     L=1
     g=1
     m=1
-    return -m*g*L*np.sin(x)
+    return -(g/L)*np.sin(x)
 def g(t,v): #x'=teta'=g(t,v)
     return v
 '''
@@ -143,19 +120,80 @@ def exact_solution(t, x0, v0):
     v = v0 * np.cos(omega * t) - (x0 * omega * np.sin(omega * t))
     return x,v'''
 
+#código separado para runge_kutta
+def modelo_hamiltoniano(t, y):
+    # Parâmetros do sistema
+    L=1
+    gr=1
+    m=1
+
+    # Variáveis de estado
+    x, p = y[:2]  # Posição e momento
+
+    # Equações de movimento para o sistema hamiltoniano
+    dxdt = p / (m*L**2)
+    dpdt = -m*gr*L*np.sin(x)
+
+    return np.array([dxdt, dpdt])
+
+def calcular_energia(y):
+    # Parâmetros do sistema
+    L=1
+    gr=1
+    m=1
+
+    # Variáveis de estado
+    x, p = y[:2]  # Posição e momento
+
+    # Cálculo da energia
+    energia = 0.5 *m* ((p/(m*L)))**2 + m*gr*L * (1-np.cos(x))
+
+
+    return energia
+
+def runge_kutta4(t0, y0, h, num_pontos):
+    t = np.zeros(num_pontos)
+    y = np.zeros((num_pontos, len(y0)))
+    energia = np.zeros(num_pontos)
+    t[0] = t0
+    y[0] = y0
+    energia[0] = calcular_energia(y0)
+
+    for i in range(1, num_pontos):
+        k1 = h * modelo_hamiltoniano(t[i-1], y[i-1])
+        k2 = h * modelo_hamiltoniano(t[i-1] + h/2, y[i-1] + k1/2)
+        k3 = h * modelo_hamiltoniano(t[i-1] + h/2, y[i-1] + k2/2)
+        k4 = h * modelo_hamiltoniano(t[i-1] + h, y[i-1] + k3)
+
+        t[i] = t[i-1] + h
+        y[i] = y[i-1] + (k1 + 2*k2 + 2*k3 + k4) / 6
+        energia[i] = calcular_energia(y[i])
+
+    return t, y, energia
+#parâmetros gerais
 t0 = 0.0
 tf = 10.0
 dt = 0.01
 x0 = 1.0
 v0 = 0.0
 
+#parâmetros para runge-kutta
+p0 = 0.0
+y0 = np.array([x0, p0])
+t_inicio = 0.0
+t_fim = 10.0
+num_pontos = 1000
+h = (t_fim - t_inicio) / num_pontos
+
+
 t_adams2, x_adams2, E_adams2 = adams_bashforth2_method(t0, tf, dt, x0, v0, f, g)
 t_adams3, x_adams3, E_adams3 = adams_bashforth3_method(t0, tf, dt, x0, v0, f, g)
 t_euler_semi, x_euler_semi, E_euler_semi = semi_implicit_euler_method(t0, tf, dt, x0, v0, f, g)
 t_euler, x_euler, E_euler = euler_method(t0, tf, dt, x0, v0, f, g)
 t_verlet, x_verlet, E_verlet = verlet_method(t0, tf, dt, x0, v0, f)
-t_rk4, x_rk4, E_rk4 = runge_kutta_method(t0, tf, dt, x0, v0, f, g)
 t_leapfrog, x_leapfrog, E_leapfrog = leapfrog_method(t0, tf, dt, x0, v0, f)
+t_rk4, y, E_rk4 = runge_kutta4(t0, y0, h, num_pontos)
+x_rk4=y[:,0]
 
 #t_exact = np.linspace(t0, tf, int((tf - t0) / dt) + 1)
 #x_exact,v_exact = exact_solution(t_exact, x0, v0)
--- seg teste.py
+++ seg teste.py
@@ -11,6 +11,30 @@ def f(t, x):
 def g(t, v):
     return v
 
+def modelo_orbita_runge_kutta(t, y):   #equações do sistema para runge-kutta
+    # Parâmetros do sistema
+    Grk = 1.0/100       # Gravitational constant
+    m1 = 100.0    # Mass of the central body (e.g., planet)
+    m2 = 1.0      # Mass of the satellite (e.g., spacecraft)
+
+    # Variáveis de estado
+    x, y, vx, vy = y[:4]  # Position and velocity components
+
+    # Distance between the two bodies
+    r = np.sqrt(x**2 + y**2)
+
+    # Gravitational force components
+    Fx = -Grk * m1 * m2 * x / r**3
+    Fy = -Grk * m1 * m2 * y / r**3
+
+    # Equations of motion for the orbital system
+    dxdt = vx
+    dydt = vy
+    dvxdt = Fx / m2
+    dvydt = Fy / m2
+
+    return np.array([dxdt, dydt, dvxdt, dvydt])
+
 def verlet_method(t0, tf, dt, x0, v0, f):
     num_steps = int((tf - t0) / dt)
     t = np.linspace(t0, tf, num_steps)
@@ -65,32 +89,6 @@ def semi_implicit_euler_method(t0, tf, dt, x0, v0, f, g):
 
     return t, x, E
 
-def runge_kutta_method(t0, tf, dt, x0, v0, f, g):
-    num_steps = int((tf - t0) / dt)
-    t = np.linspace(t0, tf, num_steps)
-    x = np.zeros((num_steps, 2))
-    v = np.zeros_like(x)
-    E = np.zeros(num_steps)
-
-    x[0] = x0
-    v[0] = v0
-    E[0] = 0.5 * m * np.linalg.norm(v0)**2 - G * m / np.linalg.norm(x0)
-
-    for i in range(1, num_steps):
-        k11 = dt*g(t[i-1], v[i-1])
-        k12 = dt*f(t[i-1], x[i-1])
-        k21 = dt*g(t[i-1] + 0.5*dt, v[i-1] + 0.5*k11)
-        k22 = dt*f(t[i-1] + 0.5*dt, x[i-1] + 0.5*k12)
-        k31 = dt*g(t[i-1] + 0.5*dt, v[i-1] + 0.5*k21)
-        k32 = dt*f(t[i-1] + 0.5*dt, x[i-1] + 0.5*k22)
-        k41 = dt*g(t[i-1] + dt, v[i-1] + k31)
-        k42 = dt*f(t[i-1] + dt, x[i-1] + k32)
-        v[i] = v[i-1] + (k12 + 2*k22 + 2*k32 + k42) / 6
-        x[i] = x[i-1] + (k11 + 2*k21 + 2*k31 + k41)  / 6
-        E[i] = 0.5 * m * np.linalg.norm(v[i])**2 - G * m / np.linalg.norm(x[i])
-
-    return t, x, E
-
 def leapfrog_method(t0, tf, dt, x0, v0, f):
     num_steps = int((tf - t0) / dt)
     t = np.linspace(t0, tf, num_steps)
@@ -141,21 +139,79 @@ def exact_solution(t, x0, v0):
         x[i] = x0 * np.cos(omega * t[i]) + v0 / omega * np.sin(omega * t[i])
     return x
 
-# Parâmetros iniciais
+#código separado para runge-kutta
+def calcular_energia_orbita_runge_kutta(y):
+    # Parâmetros do sistema
+    Grk = 1.0 /100      # Gravitational constant
+    m1 = 100.0    # Mass of the central body (e.g., planet)
+    m2 = 1.0      # Mass of the satellite (e.g., spacecraft)
+
+    # Variáveis de estado
+    x, y, vx, vy = y[:4]  # Position and velocity components
+
+    # Distance between the two bodies
+    r = np.sqrt(x**2 + y**2)
+
+    # Kinetic energy
+    kinetic_energy = 0.5 * m2 * (vx**2 + vy**2)
+
+    # Potential energy
+    potential_energy = -Grk * m1 * m2 / r
+
+    # Total energy (specific energy)
+    energy = kinetic_energy + potential_energy
+
+    return energy
+
+def runge_kutta4(t0, y0, h, num_pontos):
+    t = np.zeros(num_pontos)
+    y = np.zeros((num_pontos, len(y0)))
+    energia = np.zeros(num_pontos)
+    t[0] = t0
+    y[0] = y0
+    energia[0] = calcular_energia_orbita_runge_kutta(y0)
+
+    for i in range(1, num_pontos):
+        k1 = h * modelo_orbita_runge_kutta(t[i-1], y[i-1])
+        k2 = h * modelo_orbita_runge_kutta(t[i-1] + h/2, y[i-1] + k1/2)
+        k3 = h * modelo_orbita_runge_kutta(t[i-1] + h/2, y[i-1] + k2/2)
+        k4 = h * modelo_orbita_runge_kutta(t[i-1] + h, y[i-1] + k3)
+
+        t[i] = t[i-1] + h
+        y[i] = y[i-1] + (k1 + 2*k2 + 2*k3 + k4) / 6
+        energia[i] = calcular_energia_orbita_runge_kutta(y[i])
+
+    return t, y, energia
+
+# Parâmetros gerais
 t0 = 0.0
 tf = 10.0
-dt = 0.001
+dt = 0.01
 x0 = np.array([1.0, 0.0])
 v0 = np.array([0.0, 1.0])
+
+
+#parametros para runge kutta
+xr0 = 1.0  # Initial x-coordinate of the satellite
+yr0 = 0.0  # Initial y-coordinate of the satellite
+vx0 = 0.0  # Initial x-velocity of the satellite
+vy0 = 1.0  # Initial y-velocity of the satellite
+y0 = np.array([xr0, yr0, vx0, vy0])
+t_inicio = 0.0
+t_fim = 10.0
+num_pontos = 1000
+h = (t_fim - t_inicio) / num_pontos
+
+
 
 # Chamada das funções
 num_steps = int((tf - t0) / dt)
 t_verlet, x_verlet, E_verlet = verlet_method(t0, tf, dt, x0, v0, f)
 t_euler, x_euler, E_euler = euler_method(t0, tf, dt, x0, v0, f, g)
 t_semi_euler, x_semi_euler, E_semi_euler = semi_implicit_euler_method(t0, tf, dt, x0, v0, f, g)
-t_rk, x_rk, E_rk = runge_kutta_method(t0, tf, dt, x0, v0, f, g)
 t_leapfrog, x_leapfrog, E_leapfrog = leapfrog_method(t0, tf, dt, x0, v0, f)
 t_adams3, x_adams3, E_adams3 = adams_bashforth3_method(t0, tf, dt, x0, v0, f, g)
+t_rk, x_rk, E_rk = runge_kutta4(t0, y0, h, num_pontos)
 # Cálculo da solução exata
 t_exact = np.linspace(t0, tf, num_steps)
 x_exact = exact_solution(t_exact, x0, v0)
@@ -165,12 +221,12 @@ E_exact = 0.5 * m * np.linalg.norm(v0)**2 - G * m / np.linalg.norm(x_exact, axis
 plt.figure(figsize=(12, 8))
 plt.subplot(2, 2, 1)
 plt.plot(x_verlet[:, 0], x_verlet[:, 1],'r', label='Verlet')
-#plt.plot(x_euler[:, 0], x_euler[:, 1],'crimson', label='Euler')
-#plt.plot(x_semi_euler[:, 0], x_semi_euler[:, 1],'darkgreen', label='Euler Semi-implícito')
+plt.plot(x_euler[:, 0], x_euler[:, 1],'crimson', label='Euler')
+plt.plot(x_semi_euler[:, 0], x_semi_euler[:, 1],'darkgreen', label='Euler Semi-implícito')
 plt.plot(x_rk[:, 0], x_rk[:, 1],'lime', label='Runge-Kutta')
-#plt.plot(x_leapfrog[:, 0], x_leapfrog[:, 1],'b--', label='Leapfrog')
-#plt.plot(x_adams3[:, 0], x_adams3[:, 1],'fuchsia', label='Adams-Bashforth 3rd order')
-#plt.plot(x_exact[:, 0], x_exact[:, 1],'black' ,label='Solução Exata')
+plt.plot(x_leapfrog[:, 0], x_leapfrog[:, 1],'b--', label='Leapfrog')
+plt.plot(x_adams3[:, 0], x_adams3[:, 1],'fuchsia', label='Adams-Bashforth 3rd order')
+plt.plot(x_exact[:, 0], x_exact[:, 1],'black' ,label='Solução Exata')
 plt.title('Órbita - Posição')
 plt.xlabel('x')
 plt.ylabel('y')
